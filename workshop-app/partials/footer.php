07b00d8f5f8fcdf19e997208968e982074899512
Unitar255 <mc231025510@student.unitar.my>
2026-03-09 22:01:06 +0800

    Delete workshop-app directory

--- workshop-app/partials/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer class="site-footer">
-  <div class="container">
-    <p>&copy; <?= date('Y') ?> Car Workshop. All rights reserved.</p>
-  </div>
-</footer>
-<script src="/workshop-app/assets/js/app.js"></script>
-</body>
-</html>
